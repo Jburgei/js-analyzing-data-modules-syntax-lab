c27e6553d2b95bb25d145efadd05ec2de6751b91
Jburgei <joyburgei4@gmail.com>
2025-12-06 12:09:16 +0300

    finished lab: combineUsers function working and passing tests

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+require("datejs");
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users : [], // array that will hold all usernames
+    merge_date: "" //will hold today's date as a string
+  };
+  args.forEach((userArray) => {
+    combinedObject.users.push(...userArray); //spread operator
+  });
 
+  const today = new Date();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
 
+  return combinedObject;
+  }
+
+console.log(combineUsers(["sam", "pat"], ["john"], ["claire"]));
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
